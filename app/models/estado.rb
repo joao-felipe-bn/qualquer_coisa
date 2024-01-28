f3a42aab4e99e3e707862090d2a2d2a19da277c5
João Felipe <jfbnfelipe.jfbn@gmail.com>
2024-01-28 16:05:15 -0300

    add message to project

--- app/models/estado.rb
+++ app/models/estado.rb
@@ -1,5 +1,11 @@
 class Estado < ApplicationRecord
     def self.ransackable_attributes(auth_object = nil)
         ["created_at", "id", "id_value", "nome", "sigla", "updated_at"]
-      end    
+    end
+    
+    # validates :nome, presence: true, acceptance: { message: 'não pode ser null' }
+    validates :nome, presence: { message: 'deve ser preenchido' }
+    validates :sigla, presence: { message: 'deve ser preenchido' }
+
 end
+
